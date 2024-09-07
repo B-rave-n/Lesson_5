aaa8a2f4cb744eb493cc88ee1034e2373abd0598
Білоцерківець Сергій <sbilocerkivec@gmail.com>
2024-09-07 11:37:42 +0300

    Lesson_5.1

---
+++ 5.py
@@ -0,0 +1,20 @@
+import string
+import keyword
+strings = [i for i in string.punctuation if i != '_']
+strings.extend([' ', '__'])
+
+zminna = input('Яку назву змінної ми перевіримо?')
+while 1:
+    count_of_strings = 0
+    for i in zminna:
+        if i in strings:
+            count_of_strings = 1
+    if zminna in keyword.kwlist or count_of_strings:
+        zminna = input('Не можна використовувати спецслова та спецсимволи. Введи іншу назву змінної:')
+        continue
+    elif zminna[0].isdigit() or not zminna.islower():
+        zminna = input('Змінна не може починатися з цифри та містити великі літери. Введи іншу назву змінної:')
+        continue
+    else:
+        print("Можеш так назвати свою змінну")
+        break
